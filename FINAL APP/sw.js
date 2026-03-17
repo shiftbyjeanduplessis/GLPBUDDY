adbd5983b36b74b2cd59b362949e5579242df5db
shiftbyjeanduplessis <shiftbyjeanduplessis@gmail.com>
2026-03-17 17:02:10 +0200

    Add files via upload

--- FINAL APP/sw.js
+++ FINAL APP/sw.js
@@ -1,2 +1,3 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', event => { /* network passthrough */ });
