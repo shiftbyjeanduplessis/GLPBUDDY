be495a3a06e2439f86b724b818ec107a6ab6a716
shiftbyjeanduplessis <shiftbyjeanduplessis@gmail.com>
2026-03-18 10:32:03 +0200

    Create event-taxonomy.js

---
+++ FINAL APP/event-taxonomy.js
@@ -0,0 +1,19 @@
+export const EVENTS = {
+  ONBOARDING_COMPLETED: 'onboarding_completed',
+  SETTINGS_SAVED: 'settings_saved',
+  WEIGHT_LOGGED: 'weight_logged',
+  CHECKIN_SAVED: 'checkin_saved',
+  SYMPTOM_SAVED: 'symptom_saved',
+  HYDRATION_LOGGED: 'hydration_logged',
+  PROGRESS_PHOTO_UPLOAD_STARTED: 'progress_photo_upload_started',
+  PROGRESS_PHOTO_UPLOADED: 'progress_photo_uploaded',
+  MEAL_PREFERENCES_SAVED: 'meal_preferences_saved',
+  MEAL_PLAN_GENERATED: 'meal_plan_generated',
+  WORKOUT_STARTED: 'workout_started',
+  WORKOUT_COMPLETED: 'workout_completed',
+  WORKOUT_SET_COMPLETED: 'workout_set_completed',
+  REST_DAY_DISPLAYED: 'rest_day_displayed',
+  FEEDBACK_SUBMITTED: 'feedback_submitted',
+  PAGE_VIEW: 'page_view',
+  APP_OPENED: 'app_opened'
+};
